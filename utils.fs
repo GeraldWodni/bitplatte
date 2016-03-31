35ec7b2e20c7eb07c534c3cf40aeab23b64c6aa4
Gerald Wodni <gerald.wodni@gmail.com>
2016-03-31 17:18:47 +0200

    skeleton added

---
+++ utils.fs
@@ -0,0 +1,27 @@
+\ (c)copyright 2014 by Gerald Wodni
+\ partly taken from mecrisp examples by Mathias Koch
+
+: MOTD ." Welcome back, commander!" ;
+
+: init
+  decimal
+  cr MOTD cr ;
+
+\ missing Forth 200x words
+
+: invert  not  inline 1-foldable ;
+
+
+\ helpers
+
+: bounds over + swap ;
+
+: limits ( n1 n-min n-max -- n2 )
+	rot min max ;
+
+: cornerstone ( Name ) ( -- )
+  <builds begin here $3FF and while 0 h, repeat
+  does>   begin dup  $3FF and while 2+   repeat 
+          eraseflashfrom
+;
+
